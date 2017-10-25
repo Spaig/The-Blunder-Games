704bd7700c1dea26fb21cfadc1607fe07f2a1c3d
Spaig <charles.densem@gmail.com>
2017-10-24 22:06:57 -0500

    Add files via upload

---
+++ whygodwhy.py
@@ -0,0 +1,79 @@
+class User:
+    # team is treated as individual, will include gamemaker
+
+    def __init__(self):
+        self.penalties = 0
+        self.timePenalty = 0
+
+    def login(self, username, password):
+
+        pass
+
+    def logout(self, username):
+        pass
+
+    def answerQuestion(self, answer):
+
+        # if answer is correct, automatically provide next clue
+
+        pass
+
+    def getQuestion(self):
+        pass
+
+    def getStatus(self):
+        pass
+
+
+class Interface:
+
+    def __init__(self):
+        pass
+
+    def command(self, commandString):
+        pass
+
+
+class Game:
+    def __init__(self):
+        self.clock = 0
+        self.isActive = False
+
+    def gameStart(self):
+        pass
+
+    def gameEnd(self):
+        pass
+
+    # holds game landmarks, anything stored within the game
+
+
+class System:
+
+    # pseudo database, way to interact with lists
+    def __init__(self):
+        self.landmarks = {}  # {"landmark":[clue, question, answer]}
+        self.teams = {"admin": "kittens", }  # gamemaker hardcoded in
+
+
+'''
+speak with the Rock about these
+def getLandmark():
+pass
+def getClue():
+pass
+def getQuestion():
+pass
+def getAnswer():
+pass
+def getTeams():
+pass
+def setTeams():
+pass
+'''
+
+'''
+Notes for Rock:
+what to do for class outlines
+TDD?
+'''
